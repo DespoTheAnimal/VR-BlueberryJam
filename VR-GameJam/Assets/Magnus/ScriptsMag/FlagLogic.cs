c033e8fbdf2c8ac530b388d177a772dbef19970a
DespoTheAnimal <thedanishmagnus@gmail.com>
2023-11-30 11:00:42 +0100

    Yes

--- VR-GameJam/Assets/Magnus/ScriptsMag/FlagLogic.cs
+++ VR-GameJam/Assets/Magnus/ScriptsMag/FlagLogic.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class FlagLogic : MonoBehaviour
 {
     [SerializeField] AudioSource AS;
+    public int Player1Score = 0;
+    public int Player2Score = 0;
     public void YouHaveTheFlag(AudioClip clip)
     {
         AS.PlayOneShot(clip);
--- VR-GameJam/Assets/Magnus/ScriptsMag/PistolGoPew.cs
+++ VR-GameJam/Assets/Magnus/ScriptsMag/PistolGoPew.cs
@@ -9,24 +9,61 @@ public class PistolGoPew : MonoBehaviour
     [SerializeField] Transform shoot_point;
     [SerializeField] GameObject bullet;
     [SerializeField] AudioSource AS;
-    public AudioClip HRANG;
-    private int factor = 100;
+    public AudioClip HRANG, pistol_reload;
+    private int factor_for_bullet = 1000;
     private int time_to_delete = 5; 
+    
+    private int max_ammo = 12, current_ammo = 12;
+
+    private float time_to_reload = 3, max_time = 3;
+
     private void Start() 
     {
         XRGrabInteractable gun = GetComponent<XRGrabInteractable>();
         gun.activated.AddListener(Shoot);
-
+        AS = GameObject.Find("Audio").GetComponent<AudioSource>();
+    }
+    private void Update() 
+    {
+        if (current_ammo <= 0)
+            Reload();
     }
 
     public void Shoot(ActivateEventArgs arg)
     {
-        GameObject new_bullet = Instantiate(bullet); // Instantiate the bullet 
-        new_bullet.transform.position = shoot_point.position; 
-        new_bullet.transform.forward = shoot_point.forward;
-        new_bullet.transform.Rotate(90f, 0f, 0f);
-        new_bullet.GetComponent<Rigidbody>().AddForce(shoot_point.forward * factor);
-        AS.PlayOneShot(HRANG);
-        Destroy(new_bullet,time_to_delete);
+        if (HasAmmo())
+        {
+            GameObject new_bullet = Instantiate(bullet); // Instantiate the bullet 
+            new_bullet.transform.position = shoot_point.position; 
+            new_bullet.transform.forward = shoot_point.forward;
+            new_bullet.transform.Rotate(90f, 0f, 0f);
+            new_bullet.GetComponent<Rigidbody>().AddForce(shoot_point.forward * factor_for_bullet);
+            AS.PlayOneShot(HRANG, 0.5f);
+            current_ammo--;
+            Destroy(new_bullet,time_to_delete);
+        }
+    }
+
+    private bool HasAmmo()
+    {
+        if (current_ammo <= 0)
+        {
+            return false;
+        }
+        else
+            return true;
+
+    }
+
+    private void Reload()
+    {
+        if (!AS.isPlaying)
+            AS.PlayOneShot(pistol_reload);
+        time_to_reload -= Time.deltaTime;
+        if (current_ammo == 0 && time_to_reload <= 0)
+        {
+           current_ammo = max_ammo; 
+           time_to_reload = max_time;
+        }
     }
 }
