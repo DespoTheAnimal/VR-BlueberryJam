c1df3a0fb3376692692686d3321c4b3b758bcaad
PeterDaneholm <av28qm@student.aau.dk>
2023-12-01 12:41:30 +0100

    Update to Flag

--- VR-GameJam/Assets/Magnus/ScriptsMag/FlagLogic.cs
+++ VR-GameJam/Assets/Magnus/ScriptsMag/FlagLogic.cs
@@ -1,17 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class FlagLogic : MonoBehaviour
 {
     public Vector3 FlagSpawn = new Vector3(0f, 1.45f, 0f);
+    public XRGrabInteractable flag;
+    
     [SerializeField] AudioSource AS;
     public int Player1Score = 0;
     public int Player2Score = 0;
-
-    private void Awake() {
-         
-    }
 
     public void YouHaveTheFlag(AudioClip clip)
     {
@@ -22,17 +21,28 @@ public class FlagLogic : MonoBehaviour
         AS.PlayOneShot(clip);
     }
 
-        private void OnTriggerEnter(Collider Other){
-        if (Other.gameObject.tag == "Endzone1"){
+    private void OnTriggerEnter(Collider Other)
+    {
+        if (Other.gameObject.tag == "Endzone1")
+        {
             CaptureFlag();
-            Player1Score ++;
-        } else if (Other.gameObject.tag == "Endzone2"){
+            Player1Score++;
+            flag.interactionLayers = 1;
+
+        }
+        else if (Other.gameObject.tag == "Endzone2")
+        {
             CaptureFlag();
             Player2Score++;
+            flag.interactionLayers = 1;
         }
+        Debug.Log(flag.interactionLayers);
     }
 
-    private void CaptureFlag() {
+    private void CaptureFlag()
+    {
+        //flag.GetComponent<XRGrabInteractable>().ForceRelease();
+        flag.interactionLayers = 0;
         transform.position = FlagSpawn;
         GetComponent<Rigidbody>().velocity = Vector3.zero;
     }
