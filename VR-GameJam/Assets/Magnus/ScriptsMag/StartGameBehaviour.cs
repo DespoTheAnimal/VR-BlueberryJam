85a1df8f63ce8655c30fa4e66d6ea93396dd2f51
DespoTheAnimal <92869485+DespoTheAnimal@users.noreply.github.com>
2023-12-04 10:11:39 +0100

    Trollemanden, plz test

--- VR-GameJam/Assets/Magnus/ScriptsMag/StartGameBehaviour.cs
+++ VR-GameJam/Assets/Magnus/ScriptsMag/StartGameBehaviour.cs
@@ -15,7 +15,9 @@ public class StartGameBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        players[0] = GameObject.Find("NetworkPlayer (Clone)");
+        players[0] = GameObject.Find("NetworkPlayer(Clone)");
+        players[1] = GameObject.Find("NetworkPlayer(Clone)(Clone)");
+
         if (players.Length == 2)
         {
             StartGame();
@@ -25,6 +27,8 @@ public class StartGameBehaviour : MonoBehaviour
     private void StartGame()
     {
         players[0].transform.position = new Vector3(14.52f, 0.37f,-15.52f);
+        players[0].tag = "Player1";
         players[1].transform.position = new Vector3(-14.52f, 0.37f, 15.15f);
+        players[1].tag = "Player2";
     }
 }
