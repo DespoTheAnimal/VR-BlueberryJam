d73b909ee024faa568070dfb8efcbbc19da28842
Trollemanden <mikkel_trolle@hotmail.com>
2023-12-05 10:50:14 +0100

    jeg hænger mig selv

--- VR-GameJam/Assets/Peter/Scripts/GameManagement.cs
+++ VR-GameJam/Assets/Peter/Scripts/GameManagement.cs
@@ -2,82 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using Unity.Netcode;
 
-public class GameManagement : NetworkBehaviour
+public class GameManagement : MonoBehaviour
 {
-    private NetworkVariable<int> player1Score = new NetworkVariable<int>();
-    private NetworkVariable<int> player2Score = new NetworkVariable<int>();
-    public NetworkVariable<int> player1Kills = new NetworkVariable<int>();
-    public NetworkVariable<int> player2Kills = new NetworkVariable<int>();
-
+    private int player1Score = 0;
+    private int player2Score = 0;
     [SerializeField] private AudioSource audioSource;
-    public TMP_Text p1KillsText;
-    public TMP_Text p2KillsText;
+
+    public int player1Kills = 0;
+    public int player2Kills = 0;
+    public TMP_Text p1Kills;
+    public TMP_Text p2Kills;
 
     public GameObject Flag;
     public TMP_Text winner;
     public GameObject winnerPanel;
 
-    private void Start()
-    {
-        player1Kills.OnValueChanged += UpdatePlayer1Kills;
-        player2Kills.OnValueChanged += UpdatePlayer2Kills;
+    private void Update() {
+        player1Score = Flag.GetComponent<FlagLogic>().Player1Score;
+        player2Score = Flag.GetComponent<FlagLogic>().Player2Score;
+        p1Kills.text = player1Kills.ToString();
+        p2Kills.text = player2Kills.ToString();
+
+        Winner();
     }
 
-    private void Update()
-    {
-        if (IsServer)
-        {
-            player1Score.Value = Flag.GetComponent<FlagLogic>().Player1Score;
-            player2Score.Value = Flag.GetComponent<FlagLogic>().Player2Score;
-
-            Winner();
-        }
-
-        p1KillsText.text = player1Kills.Value.ToString();
-        p2KillsText.text = player2Kills.Value.ToString();
-    }
-
-    private void UpdatePlayer1Kills(int oldValue, int newValue)
-    {
-        p1KillsText.text = newValue.ToString();
-    }
-
-    private void UpdatePlayer2Kills(int oldValue, int newValue)
-    {
-        p2KillsText.text = newValue.ToString();
-    }
-
-    private void Winner()
-    {
-        if (player1Score.Value > 2)
-        {
+    private void Winner(){
+        if (player1Score > 2){
             audioSource.PlayOneShot(audioSource.clip);
             winnerPanel.SetActive(true);
             winner.text = "Player 1!";
             //Time.timeScale = 0;
-            // Implement additional logic for when the game ends
-        }
-        else if (player2Score.Value > 2)
-        {
+        } else if (player2Score > 2) {
             audioSource.PlayOneShot(audioSource.clip);
             winnerPanel.SetActive(true);
             winner.text = "Player 2";
-            //Time.timeScale = 0;
-            // Implement additional logic for when the game ends
+           // Time.timeScale = 0;
         }
-    }
-
-    [ServerRpc]
-    public void IncreasePlayer1KillsServerRpc()
-    {
-        player1Kills.Value++;
-    }
-
-    [ServerRpc]
-    public void IncreasePlayer2KillsServerRpc()
-    {
-        player2Kills.Value++;
     }
 }
