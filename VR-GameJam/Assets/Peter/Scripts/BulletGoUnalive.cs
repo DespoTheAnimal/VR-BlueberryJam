a0b85418cc0712ef1178e6c0319fcf99058d80ce
DespoTheAnimal <92869485+DespoTheAnimal@users.noreply.github.com>
2023-12-07 10:29:40 +0100

    Sådan der

--- VR-GameJam/Assets/Peter/Scripts/BulletGoUnalive.cs
+++ VR-GameJam/Assets/Peter/Scripts/BulletGoUnalive.cs
@@ -68,7 +68,7 @@ public class BulletGoUnalive : NetworkBehaviour
                     {
                         TeleportPlayer(xrOrigin, spawnPosition);
                     }*/
-                    TeleportPlayer(happened1,spawnPosition);
+                    TeleportPlayer(Happened2,spawnPosition);
                     break;
                 }
             }
@@ -85,7 +85,7 @@ public class BulletGoUnalive : NetworkBehaviour
                 XROrigin xrOrigin = networkObject.GetComponent<XROrigin>();
                 if (xrOrigin != null)                    
                 {
-                    TeleportPlayer(happened2, spawnPosition);
+                    TeleportPlayer(Happened2, spawnPosition);
                 }
                 break;
             }
