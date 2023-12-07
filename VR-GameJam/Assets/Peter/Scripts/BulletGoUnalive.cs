8c5e6c4711833f1d09b4466f73c2abe06c8591ff
Trollemanden <mikkel_trolle@hotmail.com>
2023-12-07 10:19:54 +0100

    pushy

--- VR-GameJam/Assets/Peter/Scripts/BulletGoUnalive.cs
+++ VR-GameJam/Assets/Peter/Scripts/BulletGoUnalive.cs
@@ -12,8 +12,8 @@ public class BulletGoUnalive : NetworkBehaviour
     {
         new Vector3(14.56f, 0.37f, -15.15f),
         new Vector3(-14.56f, 0.37f, 15.15f),
-        new Vector3(15.25f,0.37f,1327.70f),
-        new Vector3(-16.36f,0.370000005f,-14.479f)
+        new Vector3(15.25f,0.37f,14.56f),
+        new Vector3(-15.25f,0.37f,-14.56f)
     };
 
     GameObject happened;
